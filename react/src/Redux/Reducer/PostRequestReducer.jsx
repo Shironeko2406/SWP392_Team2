89fe275ee901ce930b8915634cf3c83aabdac9c2
Shironeko2406 <hieuntse161485@fpt.edu.vn>
2024-06-28 07:57:55 +0700

    update lại template admin

--- react/src/Redux/Reducer/PostRequestReducer.jsx
+++ react/src/Redux/Reducer/PostRequestReducer.jsx
@@ -2,7 +2,36 @@ import { createSlice } from "@reduxjs/toolkit";
 import { HOST_DOMAIN, TOKEN_AUTHOR, getDataTextStorage } from "../../Utils/UtilFuction";
 
 const initialState = {
-  postListUserLogin: [],
+  postListUserLogin: [
+    {
+      "postId": "efaac2b4-f5fe-4305-ae80-342296d8072a",
+      "description": "Tìm gia sư kèm thái lan lan lan",
+      "location": "quan 3",
+      "schedule": "3 session/week",
+      "preferredTime": "2h",
+      "mode": 0,
+      "gender": 2,
+      "status": 0,
+      "requestSkill": "ielts",
+      "createdBy": "545cae62-66ee-4c8f-aeee-a93d5164ff8e",
+      "createdDate": "2024-06-19T19:58:38",
+      "applies": []
+    },
+    {
+      "postId": "51353f28-6dc6-4a86-8d4b-3c063c932ed7",
+      "description": "Tìm gia sư kèm tiếng anh",
+      "location": "quan 3",
+      "schedule": "3 session/week",
+      "preferredTime": "2hour",
+      "mode": 0,
+      "gender": 1,
+      "status": 0,
+      "requestSkill": "ielts 7.",
+      "createdBy": "9ef8c67c-5bf6-4093-ba63-0d820ff38978",
+      "createdDate": "2024-06-25T23:40:38",
+      "applies": []
+    },
+  ],
   postById: {},
 };
 
